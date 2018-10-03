8cf2f166355e8ff271b040d69bdd115e296512c4
Erik Lonroth <erik.lonroth@scania.com>
2018-10-03 14:32:31 +0200

    Passing munge key works

--- requires.py
+++ requires.py
@@ -4,6 +4,10 @@ import charmhelpers.core.hookenv as hookenv
 
 
 class MungeAuthRequires(reactive.Endpoint):
+
+    def _local_munge_key(self):
+        self._cached_munge_key = hookenv.config().get('munge_key')
+    
     def _munge_auth_relation(self):
         # can only be related to a single munge provider
         assert len(self.relations) < 2
@@ -16,10 +20,11 @@ class MungeAuthRequires(reactive.Endpoint):
         hookenv.log('Joined munge provider units: {}'.format(joined_units))
         remote_unit = hookenv.remote_unit()
         hookenv.log('Remote unit: {}'.format(remote_unit))
-        remote_munge_key = joined_units[remote_unit].get('munge_key')
+        remote_munge_key = joined_units[remote_unit].received.get('munge_key')
         # update a munge key if it's different from a local one
         # there were no per-application endpoint buckets at the time of writing
         # implemented in Juju
+        self._local_munge_key()
         if remote_munge_key and remote_munge_key != self._cached_munge_key:
             self._cached_munge_key = remote_munge_key
             flags.set_flag(self.expand_name(
